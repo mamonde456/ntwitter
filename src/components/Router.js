60409b9172b9cee0d1a00c374e344e43e0579373
mamonde456 <mamonde456@gmail.com>
2022-06-03 17:01:14 +0900

    deploy

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Router, Route, Routes, BrowserRouter } from "react-router-dom";
 import Profile from "routes/Profile";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
@@ -6,40 +6,42 @@ import Navigation from "./Navigation";
 
 function AppRouter({ isLoggedIn, userObj, refreshUser }) {
   return (
-    <Router>
-      {isLoggedIn && <Navigation userObj={userObj} />}
-      <div
-        style={{
-          maxWidth: 890,
-          width: "100%",
-          margin: "0 auto",
-          marginTop: 80,
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <Routes>
-          {isLoggedIn ? (
-            <>
-              <Route
-                exact
-                path="/"
-                element={<Home userObj={userObj} />}
-              ></Route>
-              <Route
-                exact
-                path="/profile"
-                element={
-                  <Profile userObj={userObj} refreshUser={refreshUser} />
-                }
-              ></Route>
-            </>
-          ) : (
-            <Route exact path="/" element={<Auth />}></Route>
-          )}
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router>
+        {isLoggedIn && <Navigation userObj={userObj} />}
+        <div
+          style={{
+            maxWidth: 890,
+            width: "100%",
+            margin: "0 auto",
+            marginTop: 80,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Routes>
+            {isLoggedIn ? (
+              <>
+                <Route
+                  exact
+                  path="/"
+                  element={<Home userObj={userObj} />}
+                ></Route>
+                <Route
+                  exact
+                  path="/profile"
+                  element={
+                    <Profile userObj={userObj} refreshUser={refreshUser} />
+                  }
+                ></Route>
+              </>
+            ) : (
+              <Route exact path="/" element={<Auth />}></Route>
+            )}
+          </Routes>
+        </div>
+      </Router>
+    </BrowserRouter>
   );
 }
 
